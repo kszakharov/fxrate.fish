ce40237c69272110ec0e97da757279159e92ecfc
Kostiantyn Zakharov <kszakharov@gmail.com>
2026-08-02 20:04:13 -0400

    feat: add fxrate fish function

---
+++ functions/fxrate.fish
@@ -0,0 +1,27 @@
+function fxrate
+    set pair "USDCAD"
+    set base_url "https://www.bankofcanada.ca/valet/observations/FX$pair/json"
+
+    if test (count $argv) -eq 0
+        set -a argv (date -v-1d "+%Y-%m-%d")
+    end
+
+    for req_date in $argv
+        set url "$base_url?start_date=$req_date&end_date=$req_date"
+        set response (curl -s $url)
+            if test -z "$response"
+            echo "Error: No response from Bank of Canada API"
+            return 1
+        end
+
+        set value (echo $response | jq -r ".observations[0].FX$pair.v")
+        set date  (echo $response | jq -r ".observations[0].d")
+        set label (echo $response | jq -r ".seriesDetail.FX$pair.label")
+
+        if test "$value" = "null" -o -z "$value"
+            echo "$req_date → no data"
+        else
+            echo "$label: $date: $value"
+        end
+    end
+end
